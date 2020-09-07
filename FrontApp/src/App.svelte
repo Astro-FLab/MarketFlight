735c9f6337e253038f2656f33df9c012c53607c8
Marceau Meignent <marceau.meignent@neoxia.com>
2020-09-07 17:48:01 +0200

    Navbar, Flights, Darkmode

--- FrontApp/src/App.svelte
+++ FrontApp/src/App.svelte
@@ -1,13 +1,64 @@
 <script lang="ts">
+	import { Router, Link, Route } from "svelte-routing";
+	import Flights from "./Flights/Flights.svelte";
 	export let name: string;
+	export let url = "";
+
+	function toggleDarkMode() {
+		window.document.body.classList.toggle('dark-mode')
+	}
 </script>
 
+<Router url="{url}">
+	<!-- NavBar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">	
+		<span class="navbar-brand mb-0 h1 mr-4"> {name}</span>
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active mr-4">
+				<Link to="/">Home</Link>
+			</li>
+			<li class="nav-item">
+				<Link class="nav-item" to="/flights">Flights</Link>
+			</li>
+		</ul>
+		<ul class="navbar-nav">
+			<li class="nav-item ml-1">
+				<button 
+					type="button" 
+					class="btn btn-dark"
+					on:click={toggleDarkMode}
+				>
+					Toggle Dark Mode
+				</button>
+			</li>
+		</ul>	
+	</nav>
+
+	<!-- Routes -->
+	<div>
+		<Route path="flights" component="{Flights}" />
+	</div>
+</Router>
+  
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1> Welcome on {name} ! </h1>
 </main>
 
 <style>
+
+	:global(body.dark-mode) { 
+        background-color: black !important;
+        color: white;
+    }
+
+	nav {
+		opacity: 0.8;
+	}
+
+	.navbar-brand {
+		color: orange !important;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,7 +67,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: orange;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
