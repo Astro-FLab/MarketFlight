d43fc6de3b5ad16d55275a39a23d1f0b470d669e
Marceau Meignent <marceau.meignent@neoxia.com>
2020-09-08 11:29:11 +0200

    HomePage, CSS, Prettier

--- FrontApp/src/App.svelte
+++ FrontApp/src/App.svelte
@@ -1,81 +1,75 @@
 <script lang="ts">
-	import { Router, Link, Route } from "svelte-routing";
-	import Flights from "./Flights/Flights.svelte";
-	export let name: string;
-	export let url = "";
+    import { Router, Link, Route } from 'svelte-routing';
+    import Flights from './Flights/Flights.svelte';
+    import Icon from 'svelte-awesome/components/Icon.svelte';
+    import { faHome, faMoon, faPlane } from '@fortawesome/free-solid-svg-icons';
+    import HomePage from './HomePage/HomePage.svelte';
 
-	function toggleDarkMode() {
-		window.document.body.classList.toggle('dark-mode')
-	}
+    export let name: string;
+    export let url = '';
+
+    function toggleDarkMode() {
+        window.document.body.classList.toggle('dark-mode');
+    }
 </script>
 
-<Router url="{url}">
-	<!-- NavBar -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">	
-		<span class="navbar-brand mb-0 h1 mr-4"> {name}</span>
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active mr-4">
-				<Link to="/">Home</Link>
-			</li>
-			<li class="nav-item">
-				<Link class="nav-item" to="/flights">Flights</Link>
-			</li>
-		</ul>
-		<ul class="navbar-nav">
-			<li class="nav-item ml-1">
-				<button 
-					type="button" 
-					class="btn btn-dark"
-					on:click={toggleDarkMode}
-				>
-					Toggle Dark Mode
-				</button>
-			</li>
-		</ul>	
-	</nav>
-
-	<!-- Routes -->
-	<div>
-		<Route path="flights" component="{Flights}" />
-	</div>
-</Router>
-  
-<main>
-	<h1> Welcome on {name} ! </h1>
-</main>
-
 <style>
-
-	:global(body.dark-mode) { 
+    :global(body.dark-mode) {
         background-color: black !important;
         color: white;
     }
 
-	nav {
-		opacity: 0.8;
-	}
+    nav {
+        opacity: 0.8;
+    }
 
-	.navbar-brand {
-		color: orange !important;
-	}
+    .navbar-brand {
+        color: orange !important;
+    }
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: orange;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
+
+<Router {url}>
+    <!-- NavBar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1 mr-6"> {name}</span>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active mr-6 d-flex flex-center">
+                <Icon data={faHome} class="mr-2" />
+                <Link to="/">Home</Link>
+            </li>
+            <li class="nav-item">
+                <Icon data={faPlane} class="mr-2" />
+                <Link class="nav-item" to="/flights">Flights</Link>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item ml-1">
+                <button type="button" class="btn btn-dark" on:click={toggleDarkMode}>
+                    <Icon data={faMoon} />
+                    Toggle Dark Mode
+                </button>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Routes -->
+    <div>
+        <Route path="flights" component={Flights} />
+        <Route path="/" component={HomePage} />
+    </div>
+</Router>
+
+<main />
